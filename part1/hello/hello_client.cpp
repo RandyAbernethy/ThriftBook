b77097b191432d9bc6df19ec7e84d30f79754119
ra <ra@apache.org>
2014-04-04 15:03:57 -0700

    updated C++ example to C++11

--- part1/hello/hello_client.cpp
+++ part1/hello/hello_client.cpp
@@ -3,17 +3,19 @@
 #include <iostream>
 #include <string>
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 #include <thrift/transport/TSocket.h>
 #include <thrift/protocol/TBinaryProtocol.h>
 #include "gen-cpp/HelloSvc.h"
 
 using namespace apache::thrift::transport;  
 using namespace apache::thrift::protocol;   
+using boost::make_shared;
 
 int main() {
-    boost::shared_ptr<TSocket> socket(new TSocket("localhost", 8585));
+    auto socket = make_shared<TSocket>("localhost", 8585);
     socket->open();
-    boost::shared_ptr<TBinaryProtocol> protocol(new TBinaryProtocol(socket));
+    auto protocol = make_shared<TBinaryProtocol>(socket);
 
     HelloSvcClient client(protocol);
     std::string msg;
