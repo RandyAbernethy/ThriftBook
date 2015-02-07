37ac7e145b4c3edee29924209a4dd554efc3a8b6
Randy Abernethy <ra@apache.org>
2015-02-06 21:06:36 -0800

    perl example

---
+++ part3/script/perl/HelloClient.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+
+use lib './gen-perl';
+
+use Thrift::Socket;
+use Thrift::BufferedTransport;
+use Thrift::BinaryProtocol;
+use helloSvc;
+    
+my $trans_ep    = Thrift::Socket->new("localhost", 9095);
+my $trans_buf = Thrift::BufferedTransport->new($trans_ep);
+my $proto  = Thrift::BinaryProtocol->new($trans_buf);
+my $client    = helloSvcClient->new($proto);
+    
+$trans_ep->open;
+my $msg = $client->getMessage("World!");
+$trans_ep->close;
+
+print $msg."\n"
