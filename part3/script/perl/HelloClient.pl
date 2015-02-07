49679abf1ee002d9db33ad4b7004c22614f3a7d6
Randy Abernethy <ra@apache.org>
2015-02-06 21:07:48 -0800

    removed perl whitespace errors

--- part3/script/perl/HelloClient.pl
+++ part3/script/perl/HelloClient.pl
@@ -6,12 +6,12 @@ use Thrift::Socket;
 use Thrift::BufferedTransport;
 use Thrift::BinaryProtocol;
 use helloSvc;
-    
+
 my $trans_ep    = Thrift::Socket->new("localhost", 9095);
 my $trans_buf = Thrift::BufferedTransport->new($trans_ep);
 my $proto  = Thrift::BinaryProtocol->new($trans_buf);
 my $client    = helloSvcClient->new($proto);
-    
+
 $trans_ep->open;
 my $msg = $client->getMessage("World!");
 $trans_ep->close;
