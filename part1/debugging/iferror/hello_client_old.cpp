88f233754647a7e132e8a637aa044517fbbbd3d1
Randy Abernethy <ra@apache.org>
2015-02-09 15:13:46 -0800

    interface incompatibility demo

---
+++ part1/debugging/iferror/hello_client_old.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+#include <thrift/protocol/TJSONProtocol.h>
+#include "gen-cpp/HelloSvc.h"
+
+using namespace apache::thrift::transport;  
+using namespace apache::thrift::protocol;   
+using boost::shared_ptr;
+using boost::make_shared;
+
+int main() {
+    shared_ptr<TTransport> trans = make_shared<TSocket>("localhost", 9095);
+    trans = make_shared<TBufferedTransport>(trans);
+    auto proto = make_shared<TJSONProtocol>(trans);
+    auto client = make_shared<HelloSvcClient>(proto);
+
+    trans->open();
+    int msg = client->hello_func("Zaphod", "Beeblebrox");
+    std::cout << "[Client] received: " << msg << std::endl;
+}
+
---
+++ part1/debugging/iferror/hello_name_old.thrift
@@ -0,0 +1,4 @@
+service HelloSvc {
+    i32 hello_func(1: string fname, 2: string lname)
+}
+
