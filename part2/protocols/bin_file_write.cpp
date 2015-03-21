3dc47c41d77fa6f7ec10b51ccb79fe024c7cf268
Randy Abernethy <ra@apache.org>
2015-03-20 18:23:41 -0700

    cpp11 update

--- part2/protocols/bin_file_write.cpp
+++ part2/protocols/bin_file_write.cpp
@@ -3,10 +3,13 @@
 #include <iostream>
 #include <string>
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 #include <thrift/transport/TSimpleFileTransport.h>
+#include <thrift/protocol/TProtocol.h>
 #include <thrift/protocol/TBinaryProtocol.h>
 
 using namespace apache::thrift::transport;
+using namespace apache::thrift::protocol;
 
 struct Trade {
    char symbol [16];
@@ -22,28 +25,30 @@ int main()
     trade.size = 2500;
 
     const std::string path_name("data");
-    boost::shared_ptr<TTransport> trans(new TSimpleFileTransport(path_name, false, true));
-    apache::thrift::protocol::TBinaryProtocol proto(trans);     	
+    boost::shared_ptr<TTransport> trans =
+        boost::make_shared<TSimpleFileTransport>(path_name, false, true);
+    boost::shared_ptr<TProtocol> proto =
+        boost::make_shared<TBinaryProtocol>(trans);
 
     int i = 0;
-    i += proto.writeStructBegin("Trade");             			     
+    i += proto->writeStructBegin("Trade");
 
-    i += proto.writeFieldBegin("symbol ", 
+    i += proto->writeFieldBegin("symbol ",
                                ::apache::thrift::protocol::T_STRING, 1);
-    i += proto.writeString(std::string(trade.symbol));          	
-    i += proto.writeFieldEnd();                     		
+    i += proto->writeString(std::string(trade.symbol));
+    i += proto->writeFieldEnd();
 
-    i += proto.writeFieldBegin("price ", 
+    i += proto->writeFieldBegin("price ",
                                ::apache::thrift::protocol::T_DOUBLE, 2);
-    i += proto.writeDouble(trade.price);                    
-    i += proto.writeFieldEnd();                     
+    i += proto->writeDouble(trade.price);
+    i += proto->writeFieldEnd();
 
-    i += proto.writeFieldBegin("size ", ::apache::thrift::protocol::T_I32, 3);   
-    i += proto.writeI32(trade.size);                    
-    i += proto.writeFieldEnd();                     
+    i += proto->writeFieldBegin("size ", ::apache::thrift::protocol::T_I32, 3);
+    i += proto->writeI32(trade.size);
+    i += proto->writeFieldEnd();
 
-    i += proto.writeFieldStop();   				                 
-    i += proto.writeStructEnd();                        	
+    i += proto->writeFieldStop();
+    i += proto->writeStructEnd();
 
     std::cout << "Wrote " << i << " bytes to " << path_name << std::endl;
 }
