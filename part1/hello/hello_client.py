1dc62e4d0766aa413adf5c73e8224c5c1a0e12df
Randy Abernethy <ra@apache.org>
2015-06-08 01:06:27 -0700

    fixed typo in hello python client

--- part1/hello/hello_client.py
+++ part1/hello/hello_client.py
@@ -2,17 +2,17 @@
 
 import sys
 sys.path.append("gen-py")
+from hello import HelloSvc
 
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
-from hello import HelloSvc
 
 trans = TSocket.TSocket("localhost", 9095)
 trans = TTransport.TBufferedTransport(trans)
 proto = TBinaryProtocol.TBinaryProtocol(trans)
 client = HelloSvc.Client(proto)
 
-trans_ep.open()
+trans.open()
 msg = client.hello_func()
 print("[Client] received: %s" % msg)
