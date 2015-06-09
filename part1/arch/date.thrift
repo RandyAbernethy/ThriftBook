9fe60c3d08b188b4f6759eab9904fa820652f423
Randy Abernethy <ra@apache.org>
2015-06-09 05:46:48 -0700

    idl examples

---
+++ part1/arch/date.thrift
@@ -0,0 +1,6 @@
+struct Date {
+    1: i16  year,
+    2: i16  month,
+    3: i16  day,
+}
+
---
+++ part1/arch/halibut.thrift
@@ -0,0 +1,11 @@
+struct Date {
+    1: i16  year,
+    2: i16  month,
+    3: i16  day,
+}
+
+service HalibutTracking {
+    i32 GetCatchInPoundsToday(),
+    i32 GetCatchInPoundsByDate(1: Date dt, 2: double tm),
+}
+
