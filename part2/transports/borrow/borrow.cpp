f9965524d8b117e9ffe7b2edfe455ca014d4b670
Randy Abernethy <ra@apache.org>
2015-06-24 16:40:33 -0700

    added makefile to trans borrow

--- part2/transports/borrow/borrow.cpp
+++ part2/transports/borrow/borrow.cpp
@@ -1,33 +1,33 @@
 // Apache Thrift Memory Transport Borrow in C++ 
 
-#include <iostream>
 #include <thrift/transport/TBufferTransports.h>
+#include <iostream>
 
 struct Trade {
-   char symbol[16];
-   double price;
-   int size;
+    char symbol[16];
+    double price;
+    int size;
 };
 
 int main() {
-    apache::thrift::transport::TMemoryBuffer transport(4096);
+    apache::thrift::transport::TMemoryBuffer trans(4096);
 
     Trade trade;
     trade.symbol[0] = 'F'; trade.symbol[1] = '\0';
     trade.price = 13.10;
     trade.size = 2500;
-    transport.write((const uint8_t *)&trade, sizeof(trade));
+    trans.write(reinterpret_cast<const uint8_t *>(&trade), sizeof(trade));
 
     uint32_t len = sizeof(trade);
-    Trade * ptrade = (Trade *) transport.borrow(nullptr, &len);
-	if (nullptr == ptrade || sizeof(trade) > len) {
-		std::cout << "Failed to borrow a complete trade!" << std::endl;
+    auto ptrade = reinterpret_cast<const Trade *>(trans.borrow(nullptr, &len));
+    if (nullptr == ptrade || sizeof(trade) > len) {
+        std::cout << "Failed to borrow a complete trade!" << std::endl;
         return -1;
-	}
+    }
 
     std::cout << "Trade(" << len << "): " << ptrade->symbol << " " 
  			  << ptrade->size << " @ " << ptrade->price << std::endl;
 
-	transport.consume(sizeof(trade));
+    trans.consume(sizeof(trade));
 }
 
