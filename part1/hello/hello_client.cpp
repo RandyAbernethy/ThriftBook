e94b06158dfc89f3ddebb7c2020124494f3ae719
Randy Abernethy <ra@apache.org>
2015-06-24 14:09:08 -0700

    cleaning up hello

--- part1/hello/hello_client.cpp
+++ part1/hello/hello_client.cpp
@@ -1,20 +1,19 @@
 // Apache Thrift Hello World C++ Client
 
-#include <iostream>
-#include <string>
-#include <boost/shared_ptr.hpp>
-#include <boost/make_shared.hpp>
+#include "gen-cpp/HelloSvc.h"
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TBufferTransports.h>
 #include <thrift/protocol/TBinaryProtocol.h>
-#include "gen-cpp/HelloSvc.h"
+#include <boost/make_shared.hpp>
+#include <iostream>
+#include <string>
 
 using namespace apache::thrift::transport;  
 using namespace apache::thrift::protocol;   
 using boost::make_shared;
 
 int main() {
-    auto trans_ep = make_shared<TSocket>("localhost", 9095);
+    auto trans_ep = make_shared<TSocket>("localhost", 9090);
     auto trans_buf = make_shared<TBufferedTransport>(trans_ep);
     auto proto = make_shared<TBinaryProtocol>(trans_buf);
     HelloSvcClient client(proto);
@@ -23,5 +22,6 @@ int main() {
     std::string msg;
     client.hello_func(msg);
     std::cout << "[Client] received: " << msg << std::endl;
+    trans_ep->close();
 }
 
