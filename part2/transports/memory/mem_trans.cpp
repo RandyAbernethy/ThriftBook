62bf2f12488d5bb5b1948b336dc0ccba8ad02eae
Randy Abernethy <ra@apache.org>
2015-06-24 21:30:42 -0700

    added makefile and build to trans mem

--- part2/transports/memory/mem_trans.cpp
+++ part2/transports/memory/mem_trans.cpp
@@ -1,7 +1,7 @@
 // Apache Thrift Memory Transport in C++ 
 
-#include <iostream>
 #include <thrift/transport/TBufferTransports.h>
+#include <iostream>
 
 struct Trade {
    char symbol[16];
@@ -12,14 +12,11 @@ struct Trade {
 int main() {
     apache::thrift::transport::TMemoryBuffer transport(4096);
 
-    Trade trade;
-    trade.symbol[0] = 'F'; trade.symbol[1] = '\0';
-    trade.price = 13.10;
-    trade.size = 2500;
-    transport.write((const uint8_t *)&trade, sizeof(trade));
+    Trade trade{"F", 13.10, 2500};
+    transport.write(reinterpret_cast<const uint8_t *>(&trade), sizeof(trade));
 
     Trade trade_read;
-    int bytes_read = transport.read((uint8_t *)&trade_read, sizeof(trade_read));
+    int bytes_read = transport.read(reinterpret_cast<uint8_t *>(&trade_read), sizeof(trade_read));
 
     std::cout << "Trade(" << bytes_read << "): " << trade_read.symbol << " " 
  			  << trade_read.size << " @ " << trade_read.price << std::endl;
