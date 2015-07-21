e1d4f0f30d89899daf025ebc6e972a28b31f0888
Randy Abernethy <ra@apache.org>
2015-07-20 21:32:45 -0700

    java async updates

--- part3/java/async/AsyncClient.java
+++ part3/java/async/AsyncClient.java
@@ -1,27 +1,81 @@
-// Apache Thrift Async Java Client
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingSocket;
 import org.apache.thrift.TException;
+import org.apache.thrift.async.TAsyncMethodCall;
+import org.apache.thrift.async.TAsyncClientManager;
+import org.apache.thrift.async.AsyncMethodCallback;
+
+import TradeReporting.TradeHistory.AsyncClient.get_last_sale_call;
 
 public class AsyncClient {
 
-    public static void main(String[] args) {
-        TSocket trans_ep = new TSocket("localhost", 9090);
-        TFramedTransport trans = new TFramedTransport(trans_ep);
-        TBinaryProtocol proto = new TBinaryProtocol(trans);
-        TradeReporting.TradeHistory.Client client = 
-                new TradeReporting.TradeHistory.Client(proto);
+    public static void main(String[] args) 
+            throws IOException, InterruptedException, TException {
+        TAsyncClientManager client_man = new TAsyncClientManager();
+        TNonblockingSocket trans_ep = new TNonblockingSocket("localhost", 9090);
+        TradeReporting.TradeHistory.AsyncClient client = 
+                new TradeReporting.TradeHistory.AsyncClient(new TBinaryProtocol.Factory(), 
+                                                            client_man, trans_ep);
 
-        try {
-            trans.open();
-            TradeReporting.TradeReport tr = client.get_last_sale("GE");
-            System.out.println("[Client] received [" + tr.seq_num + "] " + tr.symbol + " : " + tr.size + " @ " + tr.price);
-        } catch (TException ex) {
-            System.out.println("Error: " + ex.toString());
-        }
-        trans.close();
+        final CountDownLatch latch = new CountDownLatch(1);
+        client.get_last_sale("IBM", new AsyncMethodCallback<TradeReporting.TradeHistory.AsyncClient.get_last_sale_call>() {
+            @Override
+            public void onError(Exception ex) {
+                if (ex instanceof TimeoutException) {
+                    System.out.println("Async call timed out");
+                } else {
+                    System.out.println("Async call error");
+                }
+                latch.countDown();
+            }
+
+            @Override
+            public void onComplete(TradeReporting.TradeHistory.AsyncClient.get_last_sale_call cob) {
+                try {
+                    TradeReporting.TradeReport tr = cob.getResult();
+                    System.out.println("[Client] received [" + tr.seq_num + "] " + tr.symbol + " : " + tr.size + " @ " + tr.price);
+                } catch (TException e) {
+                    ;
+                } finally {
+                    latch.countDown();
+                }
+            }
+        });
+        boolean res = latch.await(2, TimeUnit.SECONDS);
+
+        client.setTimeout(1000);
+        final CountDownLatch latch2 = new CountDownLatch(1);
+        client.get_last_sale("GE", new AsyncMethodCallback<TradeReporting.TradeHistory.AsyncClient.get_last_sale_call>() {
+            @Override
+            public void onError(Exception ex) {
+                if (ex instanceof TimeoutException) {
+                    System.out.println("Async call timed out");
+                } else {
+                    System.out.println("Async call error");
+                }
+                latch2.countDown();
+            }
+
+            @Override
+            public void onComplete(TradeReporting.TradeHistory.AsyncClient.get_last_sale_call cob) {
+                try {
+                    TradeReporting.TradeReport tr = cob.getResult();
+                    System.out.println("[Client] received [" + tr.seq_num + "] " + tr.symbol + " : " + tr.size + " @ " + tr.price);
+                } catch (TException e) {
+                    ;
+                } finally {
+                    latch2.countDown();
+                }
+            }
+        });
+        boolean calledBack = latch2.await(2, TimeUnit.SECONDS);
+        client_man.stop();
+        trans_ep.close();
     }
 }
 
--- part3/java/async/AsyncServer.java
+++ part3/java/async/AsyncServer.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.thrift.TProcessor;
@@ -9,64 +7,50 @@ import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadedSelectorServer;
 import org.apache.thrift.transport.TNonblockingServerSocket;
 import org.apache.thrift.transport.TTransportException;
+import org.apache.thrift.async.AsyncMethodCallback;
 
 
 public class AsyncServer {
-    public static class TradeHistoryHandler implements TradeReporting.TradeHistory.Iface {
-        AtomicInteger seq = new AtomicInteger(0);
 
-        public TradeReporting.TradeReport get_last_sale(String Symbol) throws org.apache.thrift.TException {
+    public static class TradeHistoryHandler implements TradeReporting.TradeHistory.Iface {
+        private AtomicInteger seq = new AtomicInteger(0);
+
+        public TradeReporting.TradeReport get_last_sale(String Symbol) 
+                throws org.apache.thrift.TException {
+            int i = seq.incrementAndGet();
             if (Symbol.equals("GE")) {
-                return new TradeReporting.TradeReport(Symbol, 50.25, 1500, seq.incrementAndGet());
+                try { Thread.sleep(10000); } catch (InterruptedException ie) {;}
+                return new TradeReporting.TradeReport(Symbol, 50.25, 1500, i);
             } else {
-                return new TradeReporting.TradeReport(Symbol, 76.50, 500, seq.incrementAndGet());
+                return new TradeReporting.TradeReport(Symbol, 76.50, 500, i);
             }
         }
     }
 
-    public static class AsyncTradeHistoryHandler implements TradeReporting.TradeHistory.AsyncIface {
+    public static class AsyncTradeHistoryHandler 
+            implements TradeReporting.TradeHistory.AsyncIface {
         TradeHistoryHandler handler = new TradeHistoryHandler();
 
         @Override
-        public void get_last_sale(String Symbol, org.apache.thrift.async.AsyncMethodCallback resultHandler) throws org.apache.thrift.TException {
+        public void get_last_sale(String Symbol, AsyncMethodCallback resultHandler)
+                throws org.apache.thrift.TException {
             resultHandler.onComplete(handler.get_last_sale(Symbol));
         }
     }
 
-    static class RunnableServer implements Runnable {
-        public RunnableServer(TServer svr) {
-            this.svr = svr;
-        }
-
-        @Override
-        public void run() {
-            svr.serve();
-        }
-
-        private TServer svr;
-    }
-
-
     public static void main(String[] args)
             throws TTransportException, IOException, InterruptedException {
 
-        TProcessor proc = new TradeReporting.TradeHistory.AsyncProcessor(new AsyncTradeHistoryHandler());
-
+        TProcessor proc = 
+            new TradeReporting.TradeHistory.AsyncProcessor(new AsyncTradeHistoryHandler());
         TNonblockingServerSocket svrTrans = new TNonblockingServerSocket(9090);
-        TServer server = new TThreadedSelectorServer(new TThreadedSelectorServer.Args(svrTrans)
+        TServer server = 
+            new TThreadedSelectorServer(new TThreadedSelectorServer.Args(svrTrans)
                 .processor(proc)
                 .protocolFactory(new TBinaryProtocol.Factory())
                 .workerThreads(6)
                 .selectorThreads(3));
-        Thread server_thread = new Thread(new RunnableServer(server), "server_thread");
-        server_thread.start();
-
-        System.out.println("[Server] press enter to shutdown> ");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
-        System.out.println("[Server] shutting down...");
-        server.stop();
-        server_thread.join();
-        System.out.println("[Server] down, exiting");
+        System.out.println("[Server] listening of port 9090");
+        server.serve();
     }
 }
